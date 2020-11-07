0bd9ef28cb79ee97df0a45dc2f1b470835832bae
adumbidiot <nathaniel.daniel23@outlook.com>
2020-11-06 17:37:10 -0800

    Switch test url

--- src/lib.rs
+++ src/lib.rs
@@ -17,3 +17,30 @@ pub use crate::{
         response::Post,
     },
 };
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const BASE: &str = "https://www.codeinwp.com/";
+
+    #[tokio::test]
+    async fn get_all_posts() {
+        let client = Client::new(BASE.parse().unwrap());
+        let ret = client.get_posts_builder().send().await.unwrap();
+        assert!(!ret.list.is_empty());
+    }
+
+    #[tokio::test]
+    async fn search_posts() {
+        let client = Client::new(BASE.parse().unwrap());
+        let ret = client
+            .get_posts_builder()
+            .search("hope")
+            .order_by(OrderBy::Relevance)
+            .send()
+            .await
+            .unwrap();
+        assert!(!ret.list.is_empty());
+    }
+}
